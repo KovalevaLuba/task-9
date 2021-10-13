8a2d1bce5da187f34e7b217ce4e343d6b9346eea
KovalevaLuba <lubakovaleva@inbox.ru>
2021-10-13 16:14:32 +0300

    task3

--- task2/script.js
+++ task2/script.js
@@ -1,11 +1,17 @@
 const consoleLog = document.querySelector('#consoleLog');
-
 consoleLog.addEventListener('click', () => {
+  
     alert('Служит для вывода информации в консоль');
+}) 
+
+
+const alertEvent = document.querySelector('#alert');
+alertEvent.addEventListener('click', () => {
+    alert('Служит для вывода информации в диалоговое окно');
 })
 
-const alert = document.querySelector('#alert');
 
-alert.addEventListener('click', () => {
-    alert('Служит для вывода информации в диалоговое окно');
+const promptEvent = document.querySelector('#prompt');
+promptEvent.addEventListener('click', () => {
+    alert('Служит для ввода информации через диалоговое окно');
 })
